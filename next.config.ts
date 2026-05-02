1206448cd750a027ffe1519dcb67163bf379f28b
Faisal <xprofaisal@gmail.com>
2026-05-02 13:24:30 +0300

    Add SEO home page

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
